40a0726c8d4708d7f9104800eb2617d0c96e4991
XENOR31000 <axel.ibanez2@lycee-pardailhan.fr>
2026-02-02 18:57:01 +0100

    dev ops small update for password reset email functionality

--- auth-service/src/main/java/com/omenaapp/auth_service/adapter/out/security/SmtpPasswordResetNotifier.java
+++ auth-service/src/main/java/com/omenaapp/auth_service/adapter/out/security/SmtpPasswordResetNotifier.java
@@ -1,5 +1,8 @@
 package com.omenaapp.auth_service.adapter.out.security;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,23 +16,31 @@ public class SmtpPasswordResetNotifier implements PasswordResetNotifierPort {
 
   private final JavaMailSender mailSender;
   private final String from;
+  private final String frontBaseUrl;
 
-  public SmtpPasswordResetNotifier(JavaMailSender mailSender,
-                                  @Value("${app.mail.from}") String from) {
+  public SmtpPasswordResetNotifier(
+      JavaMailSender mailSender,
+      @Value("${app.mail.from}") String from,
+      @Value("${app.front.base-url}") String frontBaseUrl
+  ) {
     this.mailSender = mailSender;
     this.from = from;
+    this.frontBaseUrl = frontBaseUrl;
   }
 
   @Override
   public void sendResetLink(String email, String resetToken) {
-    String link = "http://localhost:4200/reset-password?token=" + resetToken;
+    String link = frontBaseUrl + "/reset-password?token=" +
+        URLEncoder.encode(resetToken, StandardCharsets.UTF_8);
+
 
     SimpleMailMessage msg = new SimpleMailMessage();
     msg.setFrom(from);
     msg.setTo(email);
     msg.setSubject("Reset password");
-    msg.setText("Click this link to reset your password:\n" + link);
+    msg.setText("Clique ici pour réinitialiser ton mot de passe:\n" + link);
 
     mailSender.send(msg);
   }
 }
+
