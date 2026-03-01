61109328eaa49f96d5c98a1df6def0d350d0cfd2
XENOR31000 <axel.ibanez2@lycee-pardailhan.fr>
2026-03-01 04:14:57 +0100

    test

--- omena-web/src/app/app.spec.ts
+++ omena-web/src/app/app.spec.ts
@@ -10,14 +10,15 @@ describe('App', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(fixture.componentInstance).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render router outlet', async () => {
     const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
     await fixture.whenStable();
+
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, front-auth');
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
 });
