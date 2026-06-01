699c7de94e830cbb2d0ae0161399e6fa83bf0c40
XENOR31000 <axel.ibanez2@lycee-pardailhan.fr>
2026-06-01 21:13:17 +0200

    save

--- omena-web/src/app/features/clients/clients.module.ts
+++ omena-web/src/app/features/clients/clients.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
-import { ClientListComponent } from './pages/client-list/client-list.component';
-import { ClientCreateComponent } from './pages/client-create/client-create.component';
-
 @NgModule({
-  declarations: [
-    ClientListComponent,
-    ClientCreateComponent
-  ],
+  declarations: [],
   imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    RouterModule
+    CommonModule
   ]
 })
 export class ClientsModule { }
+
